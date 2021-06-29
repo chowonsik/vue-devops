a626b2858661a8a11046509f0af132a2fdaece1d
wscho94 <wscho94@gmail.com>
2021-06-29 23:50:55 +0900

    Add failed test step

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>테스트 실패 연습</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
